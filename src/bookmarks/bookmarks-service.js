76ecd660e0ee6ff9c70da80a53a76cabf2ee6243
Kristopher Williams <kristopher.williams2@gmail.com>
2019-10-31 20:37:54 -0500

    working on patch

--- src/bookmarks/bookmarks-service.js
+++ src/bookmarks/bookmarks-service.js
@@ -1,29 +1,33 @@
 const BookmarksService = {
   getAllBookmarks(knex) {
-    return knex.select('*').from('bookmarks')
+    return knex.select("*").from("bookmarks");
   },
   getById(knex, id) {
-    return knex.from('bookmarks').select('*').where('id', id).first()
+    return knex
+      .from("bookmarks")
+      .select("*")
+      .where("id", id)
+      .first();
   },
   insertBookmark(knex, newBookmark) {
     return knex
       .insert(newBookmark)
-      .into('bookmarks')
-      .returning('*')
+      .into("bookmarks")
+      .returning("*")
       .then(rows => {
-        return rows[0]
-      })
+        return rows[0];
+      });
   },
   deleteBookmark(knex, id) {
-    return knex('bookmarks')
-      .where({ id })
-      .delete()
+    return knex("bookmarks")
+      .where({id})
+      .delete();
   },
   updateBookmark(knex, id, newBookmarkFields) {
-    return knex('bookmarks')
-      .where({ id })
-      .update(newBookmarkFields)
+    return knex("bookmarks")
+      .where({id})
+      .update(newBookmarkFields);
   },
-}
+};
 
-module.exports = BookmarksService
+module.exports = BookmarksService;
